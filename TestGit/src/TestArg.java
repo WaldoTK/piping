2e01ebbc068f26a12693a1652ccd9ec00797c2a0
waldo <boeyka34@gmail.com>
2016-05-30 13:46:10 +0800

    test D

--- TestGit/src/TestArg.java
+++ TestGit/src/TestArg.java
@@ -6,7 +6,7 @@ public class TestArg {
 		
 		strs [0] = "AA";
 		strs [1] = "BB";
-		testArgs("test", strs [0],strs [1], "H");
+		testArgs("test", strs [0],strs [1], "H","D");
 		
 	}
 	public static void testArgs(String test, Object ...args) {
