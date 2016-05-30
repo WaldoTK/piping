06922daa1625fd109f2823ebd338220e02f4c9fb
waldo <boeyka34@gmail.com>
2016-05-30 13:40:04 +0800

    test arguments Oject ...

---
+++ TestGit/src/TestArg.java
@@ -0,0 +1,17 @@
+
+public class TestArg {
+	static String[] strs = new String[2];
+	
+	public static void main(String[] args) {
+		
+		strs [0] = "AA";
+		strs [1] = "BB";
+		testArgs("test", strs [0],strs [1], "H");
+		
+	}
+	public static void testArgs(String test, Object ...args) {
+		for (Object str : args) {
+			System.out.println(str);
+		}
+	}
+}
